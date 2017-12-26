be2fc09c366927d98a6c3f2119e3a2cecca3f8ae
guanweicheng <1396539283@qq.com>
2017-12-26 17:37:00 +0800

    init

---
+++ NBTableView/NBTableView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NBTableView
+//
+//  Created by admin on 26/12/2017.
+//  Copyright © 2017 guanweicheng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ NBTableView/NBTableView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NBTableView
+//
+//  Created by admin on 26/12/2017.
+//  Copyright © 2017 guanweicheng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
